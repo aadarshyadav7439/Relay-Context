c3f125929e320cf4db0d69bc82875a2693e7b9ca
Aadarsh Yadav <aadarshyadav7439@gmail.com>
2026-09-11 08:31:39 +0530

    Chrome extension messaging works

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,14 @@
 const captureBtn = document.getElementById("captureBtn");
 
-captureBtn.addEventListener("click", () => {
-  console.log("Capture Context clicked!");
+captureBtn.addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({active: true,currentWindow: true,});
+
+  chrome.tabs.sendMessage(tab.id, { type: "CAPTURE_CONTEXT" }, (response) => {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError.message);
+      return;
+    }
+
+    console.log("Relay received:", response);
+  });
 });
