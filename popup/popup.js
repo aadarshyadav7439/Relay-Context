b5b0c17453ea11b99d20e3060b3331e42ade1514
Aadarsh Yadav <aadarshyadav7439@gmail.com>
2026-09-12 08:25:44 +0530

    saving the context to localStorage for persistance

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,47 @@
 const captureBtn = document.getElementById("captureBtn");
 
+const pageTitle = document.getElementById("pageTitle");
+const pageUrl = document.getElementById("pageUrl");
+const pageText = document.getElementById("pageText");
+
 captureBtn.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({active: true,currentWindow: true,});
-
-  chrome.tabs.sendMessage(tab.id, { type: "CAPTURE_CONTEXT" }, (response) => {
+  //scraper ko cature karne ka message bhejenge taki response mil sake
+  chrome.tabs.sendMessage(tab.id, { type: "CAPTURE_CONTEXT" }, async (response) => {
     if (chrome.runtime.lastError) {
       console.error(chrome.runtime.lastError.message);
       return;
     }
+    if(!response?.success){
+      console.error("RelayContext failed to capture Page.");
+      return;
+    }
 
-    console.log("Relay received:", response);
+    const context = response.data;
+    //extracted data ko localStorage me save as capturedContext
+    await chrome.storage.local.set({
+      capturedContext: context
+    });
+
+    pageTitle.textContent = context.title;
+    pageUrl.textContent = context.url;
+    pageText.textContent = context.text;
   });
 });
+
+//loads the already capturedContentfrom local Storage if present
+async function loadSavedContext() {
+  const result = await chrome.storage.local.get("capturedContext");
+
+  if (!result.capturedContext) {
+    return;
+  }
+
+  const context = result.capturedContext;
+
+  pageTitle.textContent = context.title;
+  pageUrl.textContent = context.url;
+  pageText.textContent = context.text;
+}
+//context ui me load karwa lenge
+loadSavedContext();
